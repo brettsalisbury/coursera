dd98dd49e88149e2a3a3a594f378590915e1e897
Brett Salisbury <brett.salisbury@gmail.com>
2013-09-08 11:40:38 -0700

    Initial version of the randomized queue.

--- algorithms 1/Lecture2/src/Deque.java
+++ algorithms 1/Lecture2/src/Deque.java
@@ -3,183 +3,183 @@ import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
-	private Node<Item> head;
-	private Node<Item> tail;
+    private Node<Item> head;
+    private Node<Item> tail;
 
-	// construct an empty deque
-	public Deque() {
-		this.head = null;
-		this.tail = null;
-	}
+    // construct an empty deque
+    public Deque() {
+        this.head = null;
+        this.tail = null;
+    }
 
-	// is the deque empty?
-	public boolean isEmpty() {
-		return ((this.head == this.tail) && this.head == null);
-	}
+    // is the deque empty?
+    public boolean isEmpty() {
+        return ((this.head == this.tail) && this.head == null);
+    }
 
-	// return the number of items on the deque
-	public int size() {
-		Iterator<Item> itr = this.iterator();
-		int count = 0;
-		while (itr.hasNext() == true) {
-			count++;
-			itr.next();
-		}
+    // return the number of items on the deque
+    public int size() {
+        Iterator<Item> itr = this.iterator();
+        int count = 0;
+        while (itr.hasNext()) {
+            count++;
+            itr.next();
+        }
 
-		return count;
-	}
+        return count;
+    }
 
-	// insert the item at the front
-	public void addFirst(Item item) {
-		validateItemIsNotNull(item);
+    // insert the item at the front
+    public void addFirst(Item item) {
+        validateItemIsNotNull(item);
 
-		Node<Item> newNode = new Node<Item>(item);
-		newNode.setPrev(null);
-		newNode.setNext(this.head);
+        Node<Item> newNode = new Node<Item>(item);
+        newNode.setPrev(null);
+        newNode.setNext(this.head);
 
-		if (isEmpty()) {
-			this.tail = newNode;
-		} else {
-			this.head.setPrev(newNode);
-		}
+        if (isEmpty()) {
+            this.tail = newNode;
+        } else {
+            this.head.setPrev(newNode);
+        }
 
-		this.head = newNode;
-	}
+        this.head = newNode;
+    }
 
-	private void validateItemIsNotNull(Item item) {
-		if (item == null) {
-			throw new NullPointerException();
-		}
-	}
+    private void validateItemIsNotNull(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+    }
 
-	// insert the item at the end
-	public void addLast(Item item) {
-		validateItemIsNotNull(item);
+    // insert the item at the end
+    public void addLast(Item item) {
+        validateItemIsNotNull(item);
 
-		Node<Item> newNode = new Node<Item>(item);
-		newNode.setNext(null);
-		newNode.setPrev(this.tail);
+        Node<Item> newNode = new Node<Item>(item);
+        newNode.setNext(null);
+        newNode.setPrev(this.tail);
 
-		if (isEmpty()) {
-			this.head = newNode;
-		} else {
-			this.tail.setNext(newNode);
-		}
+        if (isEmpty()) {
+            this.head = newNode;
+        } else {
+            this.tail.setNext(newNode);
+        }
 
-		this.tail = newNode;
-	}
+        this.tail = newNode;
+    }
 
-	// delete and return the item at the front
-	public Item removeFirst() {
-		validateQueueIsNotEmpty();
+    // delete and return the item at the front
+    public Item removeFirst() {
+        validateQueueIsNotEmpty();
 
-		Node<Item> removed = this.head;
+        Node<Item> removed = this.head;
 
-		this.head = removed.getNext();
+        this.head = removed.getNext();
 
-		if (this.head != null) {
-			this.head.setPrev(null);
-		} else {
-			this.tail = null;
-		}
+        if (this.head != null) {
+            this.head.setPrev(null);
+        } else {
+            this.tail = null;
+        }
 
-		return removed.getValue();
-	}
+        return removed.getValue();
+    }
 
-	private void validateQueueIsNotEmpty() {
-		if (this.isEmpty()) {
-			throw new NoSuchElementException();
-		}
-	}
+    private void validateQueueIsNotEmpty() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+    }
 
-	// delete and return the item at the end
-	public Item removeLast() {
-		validateQueueIsNotEmpty();
+    // delete and return the item at the end
+    public Item removeLast() {
+        validateQueueIsNotEmpty();
 
-		Node<Item> removed = this.tail;
+        Node<Item> removed = this.tail;
 
-		this.tail = removed.getPrev();
+        this.tail = removed.getPrev();
 
-		if (this.tail != null) {
-			this.tail.setNext(null);
-		} else {
-			this.head = null;
-		}
+        if (this.tail != null) {
+            this.tail.setNext(null);
+        } else {
+            this.head = null;
+        }
 
-		return removed.getValue();
-	}
+        return removed.getValue();
+    }
 
-	@Override
-	// return an iterator over items in order from front to end
-	public Iterator<Item> iterator() {
-		return new DequeItr<Item>(this.head);
-	}
+    @Override
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return new DequeItr<Item>(this.head);
+    }
 
-	private class Node<Item> {
+    private class Node<Item> {
 
-		private Node<Item> next;
-		private Node<Item> prev;
-		private final Item value;
+        private Node<Item> next;
+        private Node<Item> prev;
+        private final Item value;
 
-		public Node(Item value) {
-			this.next = null;
-			this.prev = null;
-			this.value = value;
-		}
+        public Node(Item value) {
+            this.next = null;
+            this.prev = null;
+            this.value = value;
+        }
 
-		public void setNext(Node<Item> next) {
-			this.next = next;
-		}
+        public void setNext(Node<Item> n) {
+            this.next = n;
+        }
 
-		public Node<Item> getNext() {
-			return this.next;
-		}
+        public Node<Item> getNext() {
+            return this.next;
+        }
 
-		public void setPrev(Node<Item> prev) {
-			this.prev = prev;
-		}
+        public void setPrev(Node<Item> p) {
+            this.prev = p;
+        }
 
-		public Node<Item> getPrev() {
-			return this.prev;
-		}
+        public Node<Item> getPrev() {
+            return this.prev;
+        }
 
-		public Item getValue() {
-			return this.value;
-		}
-	}
+        public Item getValue() {
+            return this.value;
+        }
+    }
 
-	private class DequeItr<Item> implements Iterator<Item> {
+    private class DequeItr<Item> implements Iterator<Item> {
 
-		private Node<Item> current;
+        private Node<Item> current;
 
-		public DequeItr(Node<Item> head) {
-			this.current = head;
-		}
+        public DequeItr(Node<Item> head) {
+            this.current = head;
+        }
 
-		@Override
-		public boolean hasNext() {
-			return this.current != null;
-		}
+        @Override
+        public boolean hasNext() {
+            return this.current != null;
+        }
 
-		@Override
-		public Item next() {
-			validateNextIsValidOperaton();
+        @Override
+        public Item next() {
+            validateNextIsValidOperaton();
 
-			Item value = current.getValue();
-			current = current.getNext();
-			return value;
-		}
+            Item value = current.getValue();
+            current = current.getNext();
+            return value;
+        }
 
-		private void validateNextIsValidOperaton() {
-			if (!hasNext()) {
-				throw new NoSuchElementException();
-			}
-		}
+        private void validateNextIsValidOperaton() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+        }
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
-	}
+    }
 }
--- algorithms 1/Lecture2/src/RandomizedQueue.java
+++ algorithms 1/Lecture2/src/RandomizedQueue.java
@@ -1,40 +1,126 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-	// construct an empty randomized queue
-	public RandomizedQueue() {
+    private Object[] collection;
+    private int size;
+    private int next;
 
-	}
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        this.size = 2;
+        this.next = 0;
+        this.collection = new Object[size];
+    }
 
-	// is the queue empty?
-	public boolean isEmpty() {
-		return false;
-	}
+    // is the queue empty?
+    public boolean isEmpty() {
+        return (this.next == 0);
+    }
 
-	// return the number of items on the queue
-	public int size() {
-		return -1;
-	}
+    // return the number of items on the queue
+    public int size() {
+        return this.next;
+    }
 
-	// add the item
-	public void enqueue(Item item) {
+    // add the item
+    public void enqueue(Item item) {
+        validateItemIsNotNull(item);
 
-	}
+        if (this.next == this.size) {
+            increaseCollectionSize();
+        }
 
-	// delete and return a random item
-	public Item dequeue() {
-		return null;
-	}
+        this.collection[this.next] = item;
+        this.next++;
+    }
 
-	// return (but do not delete) a random item
-	public Item sample() {
-		return null;
-	}
+    private void validateItemIsNotNull(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+    }
 
-	@Override
-	// return an independent iterator over items in random order
-	public Iterator<Item> iterator() {
-		return null;
-	}
+    private void increaseCollectionSize() {
+        int newSize = this.size * 2;
+        Object newCollection[] = new Object[newSize];
+        for (int i = 0; i < this.next; i++) {
+            newCollection[i] = this.collection[i];
+        }
+
+        this.collection = newCollection;
+        this.size = newSize;
+    }
+
+    // delete and return a random item
+    public Item dequeue() {
+        validateQueueIsNotEmpty();
+
+        int removeIndex = getIndex();
+        @SuppressWarnings("unchecked")
+        Item val = (Item) this.collection[removeIndex];
+        for (int i = removeIndex + 1; i < this.next; i++) {
+            this.collection[i - 1] = this.collection[i];
+        }
+
+        return val;
+    }
+
+    private void validateQueueIsNotEmpty() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    private int getIndex() {
+        return StdRandom.uniform(this.next);
+    }
+
+    // return (but do not delete) a random item
+    @SuppressWarnings("unchecked")
+    public Item sample() {
+        validateQueueIsNotEmpty();
+
+        int sampleIndex = getIndex();
+        return (Item) this.collection[sampleIndex];
+    }
+
+    @Override
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator(this.collection, this.next);
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+
+        private final Object[] collection;
+        private final int size;
+        private int currIndex;
+
+        public RandomizedQueueIterator(Object[] collection, int size) {
+            this.collection = collection;
+            this.size = size;
+            this.currIndex = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.currIndex < this.size;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Item next() {
+            Item val = (Item) this.collection[this.currIndex];
+            this.currIndex++;
+            return val;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
 }
